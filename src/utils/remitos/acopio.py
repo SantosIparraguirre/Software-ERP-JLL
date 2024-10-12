9e6e99758660a78bb66df34eb2e0aef2f54b9612
SantosIparraguirre <santosrp95@hotmail.com>
2024-10-12 17:38:09 -0300

    fix acopio

--- src/utils/remitos/acopio.py
+++ src/utils/remitos/acopio.py
@@ -7,6 +7,7 @@ from tkinter import messagebox
 def agregar_a_acopio(carrito, cliente):
     # Si no se seleccionó ningún cliente, anular la operación
     if not cliente:
+        messagebox.showerror("Error", "Selecciona un cliente.")
         return
 
     # Obtener el ID del cliente seleccionado en la DB
@@ -37,13 +38,6 @@ def descontar_de_acopio(carrito, cliente, imprimir):
     # Si no se seleccionó ningún cliente, anular la operación
     if not cliente:
         messagebox.showerror("Error", "Selecciona un cliente.")
-        return
-    
-    # Mensaje de confirmación
-    confirmacion = messagebox.askyesno("Confirmar", "¿Deseas descontar los productos del acopio?")
-
-    # Si el usuario no confirma, anular la operación
-    if not confirmacion:
         return
 
     # Obtener el ID del cliente seleccionado en la DB
